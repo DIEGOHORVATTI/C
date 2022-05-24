049be5a4a19183a295c92797e7be831abeae89df
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-05-24 06:22:04 -0400

    add: ponteiros

--- ponteiros.c
+++ ponteiros.c
@@ -2,10 +2,15 @@
 
 int main(void){
   
-  int valor_1 = 1;
-
-  printf("valor de 1: %d\n", valor_1);
-
+  /* int valor_1 = 1;  //valor
+  int *valor_2;     //ponteiro
+  valor_2 = &valor_1;
+  printf("Valor valor_1: %d\n", valor_1);      //valor de valor_1
+  printf("\nMemória valor_1: %p\n", &valor_1); //endereço de valor_1
+  printf("\nMemória valor_1: %p\n", valor_2);  //endereço de valor_1 apontado
+  printf("\nValor valor_1: %d\n", *valor_2);   //valor valor_2 apontado
+  printf("\nMemória valor_2: %p\n", &valor_2); //endereço de valor_2
+ */
 
   return(0);
 }
