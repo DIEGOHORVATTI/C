2c714fb4b6cc9db29604162bbd0ee2f9cbc11627
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-05 02:26:17 -0400

    add: 13.c

--- ponteiros/13.c
+++ ponteiros/13.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 /*
-  Crie um programa que contenha um array de inteiros com 10 elementos. 
-  Imprima o elemento e o endereço de cada posição do array.
+  Considere a seguinte declaração: int A, *B, **C, ***D
+  Sendo A um inteiro exiba o seu dobro, 
+  o triplo e o quádruplo desse valor 
+  utilizando apenas os ponteiros B, C e D. 
+  O ponteiro B deve ser usada para calcular o dobro, C
+  o triplo e D o quádruplo.
 */
-#define TAM 10
 
 int main(){
 
-  int vet[TAM] = {4, 6, 8, 9, 7, 2, 7, 3, 1, 5};
+  int A, *B, **C, ***D;
 
-  //debug de posições
-  for (int i = 0; i < TAM; i++) printf(" %d", i); printf("\n");
-  for (int i = 0; i < TAM; i++) printf(" ↓");  printf("\n");
-  for (int i=0; i < TAM; i++) printf(" %d", *(vet+i));
+  A = 5;
+  B = &A;
+  C = &B;
+  D = &C;
 
-  printf("\n\n");
-  for (int i = 0; i < TAM; i++){ printf(" [%d]-> %p\n", i, vet+i);}
-  printf("\n");
+  printf(" A normal: %d", A);
+  printf("\n A dobro: %d", 2 * *B);
+  printf("\n A triplo: %d", 3 * **C);
+  printf("\n A quádruplo: %d", 4 * ***D);
 
   return 0;
 }
