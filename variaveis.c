977f5bbe0c46b13d1d1a04a4ceddfba39a0029a4
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-07 10:09:33 -0400

    fix: kkkkk q porra era essa

--- variaveis.c
+++ variaveis.c
@@ -1,56 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main(){
 
   //string
-  printf( "Hello word!!!\n" );
+  printf("Hello word!!!\n\n");
 
   //int
   int a = 2;
   int b = 2;
-  /* 
-  Input
-    scanf("%d", &a);
-    scanf("%d", &b); 
-  */
+  int c = pow(2,3);
   
   //operadores aritiméticos
   printf( "a + b = %d\n", a+b );
   printf( "a - b = %d\n", a-b );
   printf( "a * b = %d\n", a*b );
   printf( "a / b = %d\n", a/b );
-
-  //a)
-  printf( "=> %d\n", 5+9+7+8/4 );
-  printf( "=> %d\n", 5+9+7+2 );
-
-  //b)
-  printf( "=> %d\n", 1-4*3/6 ); 
-
-  /* int c = pot(2,3);
-  printf( "pot(a,b) = %d\n", c); */
-
-  printf( "=> %d\n", 1-4*3/6 );
-
-  //a)
-  if ( 2*4==24/3 ) 
-     printf("%d\n", 2*4); 
-  printf("\n\n------------------\n\n");
-
-
-  //a)
-  int A = 2;
-  int B = 7;
-  float C = 3.5;
-
-  printf("%d\n", B==A*C);
+  printf( "\npow(c) = %d\n", c);
   
   return 0;
 }
-
-/*
-
-
-
-*/   
