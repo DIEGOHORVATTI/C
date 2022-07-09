714ed51160a782070f9845f822295bcfdabdb275
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-09 16:51:28 -0400

    [09/07/2022] [16:51] -- {190, sábado}

--- alocacao/5.c
+++ alocacao/5.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 //matriz dinamica
 
 int main(){
-
+    
+  srand(time(NULL));
+    
   int **mat;
 
   mat = malloc(4 * sizeof(int*));
 
+  for(int i = 0; i < 4; i++)
+    mat[i] = malloc(3 * sizeof(int));
 
-  for(int i = 0; i < 4; i++){
-    mat[i] = malooc();
+  for (int j = 0; j < 4; j++){
+    for (int i = 0; i < 4; i++)
+      mat[i][j] = 10 +(rand() % 40);
   }
 
-  return 0;
+  for (int j = 0; j < 4; j++){
+    for (int i = 0; i < 4; i++)
+      printf(" %d", mat[i][j]);
+    printf("%n");
+  }
+
+  return(0);
 }
