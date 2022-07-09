22d862d40efda8dcea74cb91beac51e9a758a57a
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-09 17:13:33 -0400

    [09/07/2022] [17:13] -- {190, sábado}

--- alocacao/6.c
+++ alocacao/6.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX_ESTUDANTES 3
 //Como ler a nota de 3 estudantes e imprimir uma tabela?
 
+typedef struct{
+  char nomes[3][30];
+  float nota;
+}sEstudante;
+
 int main(){
-    
-  struct{
-    
-  }sEstudante;
+  
+  sEstudante estudante[3];
+
+  printf("\n---| Digite a nome e nota d estudante |---\n");
+
+  for (int i=0; i < MAX_ESTUDANTES; i++){
+    printf("\nDigite nome estudante[%d]", i);
+    fgets(estudante[i].nomes, 30, stdin);
+
+    printf("\nDigite nota estudante[%d]", i);
+    scanf("%f", estudante[i].nota);
+  }
+
+  for (int i = 0; i < MAX_ESTUDANTES; i++){
+    printf("\nDigite nome estudante[%d]", i);
+    fgets(estudante[i].nomes, 30, stdin);
+
+    printf("\nDigite nota estudante[%d]", i);
+    scanf("%f", estudante[i].nota);
+  }
 
   return(0);
 }
