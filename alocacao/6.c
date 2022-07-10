382ea8dc5c65a05632b970e1bcd2b881fb275883
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-09 21:10:29 -0400

    [09/07/2022] [21:10] -- {190, sábado}

--- alocacao/6.c
+++ alocacao/6.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define MAX_ESTUDANTES 3
-//Como ler a nota de 3 estudantes e imprimir uma tabela?
+
+/*
+  Foram digitadas três linhas, onde cada uma das linhas contém o nome e a nota de
+  um aluno. Escreva um programa que leia essas informações e monte uma tabela onde
+  a primeira coluna é o nome e a segunda coluna é a nota.
+*/
 
 typedef struct{
-  char nomes[3][30];
+  char nome[MAX_ESTUDANTES][50];
   float nota;
 }sEstudante;
 
 int main(){
   
-  sEstudante estudante[3];
-
-  printf("\n---| Digite a nome e nota d estudante |---\n");
-
-  for (int i=0; i < MAX_ESTUDANTES; i++){
-    //printf("\nDigite nome estudante[%d]: ", i);
-    //fgets( estudante[i].nomes, 30, stdin);
-
-    printf("\nDigite nota estudante[%d]: ", i);
-    scanf("%f", estudante[i].nota);
-  }
+  sEstudante estudantes[MAX_ESTUDANTES];
 
   for (int i = 0; i < MAX_ESTUDANTES; i++){
-    printf("\n%s\t%.2f", estudante[i].nomes, estudante[i].nota);
+    printf(" Estudante[%d] [nome nota]: ", i+1);
+    scanf("%s%f", &estudantes[i].nome, &estudantes[i].nota);
+  }
+
+  printf("\n\tNome\tNota\n");
+  for (int i = 0; i < MAX_ESTUDANTES; i++){
+    printf("\t%s\t%.2f\n", estudantes[i].nome, estudantes[i].nota);
   }
 
   return(0);
