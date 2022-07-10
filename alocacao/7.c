fdc328288a42126475a57ca75b734ec26a1fa881
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-09 22:12:34 -0400

    [09/07/2022] [22:12] -- {190, sábado}

--- alocacao/7.c
+++ alocacao/7.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-//inverter string
+// inverter string
 
 void inverter(char vet[]){
   int fim, aux, tam = strlen(vet);
@@ -16,14 +17,33 @@ void inverter(char vet[]){
 
 }
 
-int main(){
+
+char* inverter_2(char vet[]){
+  int i, fim, aux, tam = strlen(vet);
+  char *novo = malloc( (tam+1) * sizeof(char) );
+
+  fim = tam-1;
+  for (i = 0; i < tam; i++){
+    novo[fim] = vet[i];
+    fim--;
+  }
+
+  novo[i] = '\0';
+  return(novo);
   
-  char palavra[25] = {"Muito bem feito!"};
+}
 
-  printf("\n%s", palavra);
+int main(){
+
+  char *p, palavra[25] = {"Muito bem feito!"};
+
+  printf("\n %s", palavra);
   inverter(palavra);
-
-  printf("\n%s", palavra);
+  printf("\n %s", palavra);
+  
+  printf("\n\n %s", palavra);
+  p = inverter_2(palavra);
+  printf("\n %s", p);
 
   return(0);
 }
