0415fdb0ad3510b1270e98196c1f626be8da45c2
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-10 23:12:57 -0400

    [10/07/2022] [23:12] -- {191, domingo}

---
+++ arquivo/8.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+  w  -> Escrita (cria o arquivo caso ele não exista)
+  r  -> leitura
+  a  -> anexar (adicionar informação ao final de um arquivo que já existe)
+  r+ -> leitura e escrita
+  w+ -> leitura e escrita (apaga o conteúdo caso o arquivo exista)
+  a+ -> leitura e escrita (adiciona ao final do arquivo)
+*/
+
+// escrever arquivo
+// ler arquivo
+// Organizando nosso código em procedimentos
+
+// fprintf escrever no arquivo
+// ponteiro de arquivo, typos de dados, dados 
+
+void escrever(char f[]) {
+
+  FILE *file = fopen(f, "w");
+  char nome[100];
+  int opcao, idade;
+  float altura;
+
+  if(file) {
+      do{
+        printf("\n Digite nome: ");
+        scanf("%99[^\n]", nome);
+
+        printf(" Digite idade: ");
+        scanf("%i", &idade);
+
+        printf(" Digite altura: ");
+        scanf("%f", &altura);
+
+        fprintf(file, "%s %d %.2f\n", nome, idade, altura);
+        printf("\n Inserir novo registro [0]não, [1]sim: ");
+        scanf("%d", &opcao);
+
+        getchar();
+
+      }while(opcao == 1);
+
+      fclose(file);
+
+  }else{
+    printf("\n ERRO ao abrir arquivo!\n");
+  }
+}
+
+void ler(char arquivo[]){
+
+  FILE *file = fopen(arquivo, "w");
+
+  if(file){
+
+    
+    
+  }else{
+    printf("\n ERRO ao abrir arquivo!\n");
+  }
+
+}
+
+int main() {
+
+  char nome[] = {"./texto.txt"};
+
+  escrever(nome);
+
+  return 0;
+}
